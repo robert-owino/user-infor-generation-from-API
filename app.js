eb333d61be730b0afda0ccc2972d092ca1a80bb0
robert owino <97714329+robert-owino@users.noreply.github.com>
2023-07-05 13:00:37 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+
+import get from "./utils/getElement.js";
+import getUser from "./utils/fetchUser.js";
+import displayUser from "./utils/displayUser.js";
+
+
+const btn = get(".btn");
+
+const showUser = async () => {
+  //get user from the api
+  const person = await getUser();
+  displayUser(person);
+
+  //display user
+};
+
+window.addEventListener("DOMContentLoaded", showUser);
+btn.addEventListener("click", showUser);
